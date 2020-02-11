2b5bb853dfdaf094650626ba19f4da948b60720e
SX <sx@minetest>
2020-02-12 00:33:12 +0200

    Add callbacks, wip moving jail stuff to jail.lua

--- hooks.lua
+++ hooks.lua
@@ -1,4 +1,52 @@
 
+beerchat.cb = {} -- all custom callbacks
+
+beerchat.cb.before_send 	= {} -- executed before sending message
+beerchat.cb.before_receive 	= {} -- executed before receiving message
+beerchat.cb.before_join 	= {} -- executed before channel is joined
+beerchat.cb.before_leave 	= {} -- executed before channel is leaved
+beerchat.cb.before_invite 	= {} -- excuted before channel invitation takes place
+beerchat.cb.on_forced_join 	= {} -- executed right after player is forced to channel
+
+beerchat.register_callback = function(trigger, fn)
+	if type(fn) ~= 'function' then
+		print('Error: Invalid fn argument for beerchat.register_callback, must be function')
+		return
+	end
+	if type(trigger) ~= 'string' then
+		print('Error: Invalid trigger argument for beerchat.register_callback, must be string')
+		return
+	end
+	
+	local cb = beerchat.cb
+	local callback_key = trigger
+	
+	if not cb[callback_key] then
+		print('Error: Invalid callback trigger event, possible triggers:')
+		for k,_ in pairs(cb) do
+			print(' ->   ' .. k)
+		end
+		return
+	end
+		
+	table.insert(cb[callback_key], fn)
+end
+
+beerchat.execute_callbacks = function(trigger)
+	local cb_list = beerchat.cb[trigger]
+	if not cb_list then
+		print('Error: Invalid trigger argument for beerchat.execute_callbacks')
+		-- This is internal error / dev error, stop processing current event
+		return false
+	end
+	for _,fn in ipairs(cb_list) do
+		if not fn(unpack(arg) then
+			return false
+		end
+	end
+	return true
+end
+
 -- called on every channel message
 -- params: channel, playername, message
 beerchat.on_channel_message = function()
--- jail.lua
+++ jail.lua
@@ -1,3 +1,7 @@
+-- Jail channel is where you put annoying missbehaving users with /force2channel
+beerchat.jail_channel_name = minetest.settings:get("beerchat.jail_channel_name") or "grounded"
+beerchat.jail_channel_name = beerchat.jail_channel_name or "grounded"
+
 beerchat.jail_list = {}
 
 if nil == beerchat.jail_channel_name or "" == beerchat.jail_channel_name then
@@ -7,3 +11,43 @@ end
 beerchat.is_player_jailed = function(name)
 	return true == beerchat.jail_list[name]
 end
+
+beerchat.register_callback('before_invite', function(sender, recipient, channel)
+	if beerchat.is_player_jailed(player_name) then
+		return false, player_name .. " is in chat-jail, no inviting."
+	end
+	return true
+end)
+
+beerchat.register_callback('before_mute', function(name, target)
+	if beerchat.is_player_jailed(name) then
+		return false, "You are in chat-jail, no muting for you."
+	end
+	return true
+end)
+
+beerchat.register_callback('before_join', function(name, channel)
+	if beerchat.is_player_jailed(name) then
+		return false, "You are in chat-jail, no joining channels for you."
+	end
+	return true
+end)
+
+beerchat.register_callback('before_leave', function(name, channel)
+	if beerchat.is_player_jailed(name) then
+		return false, "You are in chat-jail, no leaving for you."
+	end
+	return true
+end)
+
+beerchat.register_callback('on_forced_join', function(name, target, channel, target_meta)
+	-- going to/from jail?
+	if channel == beerchat.jail_channel_name then
+		target_meta:set_int("beerchat:jailed", 1)
+		beerchat.jail_list[target] = true
+	elseif beerchat.is_player_jailed(target) then
+		target_meta:set_int("beerchat:jailed", 0)
+		beerchat.jail_list[target] = nil
+	end
+	return true
+end)
