94a046489251aabff4d7078d33e8941f0e35176d
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-03-09 12:59:14 +0100

    fix logout issue

--- web/logout.lua
+++ web/logout.lua
@@ -3,14 +3,8 @@
 minetest.register_chatcommand("logout", {
 	description = "logs out the remote user (irc, discord)",
 	func = function(name)
-    for key, mapped_username in pairs(beerchat.remote_username_map) do
-      if mapped_username == name then
-        beerchat.remote_username_map[key] = nil
-        beerchat.save_remote_usernames()
-        return true, "Logged out!"
-      end
-    end
-
-    return false, "No user found to log out!"
+    beerchat.remote_username_map[name] = nil
+    beerchat.save_remote_usernames()
+    return true, "Logged out!"
   end
 })
