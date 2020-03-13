0ffb2c4e34181caabc4c5881fa16b43a2a9bb8c3
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-03-13 18:03:07 +0100

    fix crash if request from proxy is empty

--- web/rx.lua
+++ web/rx.lua
@@ -52,8 +52,14 @@ recv_loop = function()
 		url = beerchat.url,
 		timeout = 30,
 	}, function(res)
-		if res.succeeded and res.code == 200 then
+		if res.succeeded and res.code == 200 and res.data then
 			local data = minetest.parse_json(res.data)
+			if not data then
+				minetest.log("error", "[beerchat] content parsing error: " .. dump(res.data))
+				minetest.after(5, recv_loop)
+				return
+			end
+
 			if #data > 0 then
 				-- array received
 				for _, item in ipairs(data) do
