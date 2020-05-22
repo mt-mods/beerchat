d344434011f73f27f480d92f91148ed81e13db2e
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-05-22 14:02:39 +0200

    dev stuff

---
+++ dev/start.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+docker run --rm -it \
+	-u root:root \
+	-v $(pwd)/minetest.conf:/etc/minetest/minetest.conf \
+	-v $(pwd)/../:/root/.minetest/worlds/world/worldmods/beerchat \
+	-v mail_world:/root/.minetest/worlds/world/ \
+	--network host \
+	registry.gitlab.com/minetest/minetest/server:5.2.0
