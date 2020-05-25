414ad0ac34f7c56219e94248bc76c1b35ee644c7
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-05-25 13:18:06 +0200

    rewrite whispering code, notify player if no one heard the whispering
    fixes #13

--- whisper.lua
+++ whisper.lua
@@ -30,18 +30,28 @@ beerchat.whisper = function(name, message)
 			if cb_message then return false, cb_message else return false end
 		end
 
-		local pl = minetest.get_player_by_name(name)
-		local pl_pos = pl:getpos()
-		local all_objects = minetest.get_objects_inside_radius(
-			{ x = pl_pos.x, y = pl_pos.y, z = pl_pos.z }, radius)
+		-- true if someone heard the player
+		local successful = false
+		local player = minetest.get_player_by_name(name)
+		for _, other_player in ipairs(minetest.get_connected_players()) do
+			-- calculate distance
+			local opos = other_player:get_pos()
+			local ppos = player:get_pos()
+			local distance = vector.distance(opos, ppos)
 
-		for _,player in ipairs(all_objects) do
-			if player:is_player() then
-				local target = player:get_player_name()
+			if distance < radius then
+				-- player in range
+				local target = other_player:get_player_name()
+
 				-- Checking if the target is in this channel
-				if beerchat.is_player_subscribed_to_channel(target,
-					beerchat.main_channel_name) then
+				if beerchat.is_player_subscribed_to_channel(target, beerchat.main_channel_name) then
+
+					-- check if muted
 					if not beerchat.has_player_muted_player(target, name) then
+						-- mark as sent
+						successful = true
+
+						-- deliver message
 						beerchat.send_message(
 							target,
 							beerchat.format_message(whisper_string, {
@@ -57,9 +67,13 @@ beerchat.whisper = function(name, message)
 				end
 			end
 		end
-		return true
+
+		if successful then
+			return true
+		else
+			return false, "no one heard you whispering!"
+		end
 	end
 end
 
 minetest.register_on_chat_message(beerchat.whisper)
-
