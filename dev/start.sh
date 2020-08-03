9700fc20e67fe41e2179de5028d409e0f07c9dff
BuckarooBanzay <BuckarooBanzay@users.noreply.github.com>
2020-08-03 11:36:13 +0200

    fix volume name

--- dev/start.sh
+++ dev/start.sh
@@ -4,6 +4,6 @@ docker run --rm -it \
 	-u root:root \
 	-v $(pwd)/minetest.conf:/etc/minetest/minetest.conf \
 	-v $(pwd)/../:/root/.minetest/worlds/world/worldmods/beerchat \
-	-v mail_world:/root/.minetest/worlds/world/ \
+	-v beerchat_world:/root/.minetest/worlds/world/ \
 	--network host \
 	registry.gitlab.com/minetest/minetest/server:5.2.0
