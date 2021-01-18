998150ac38b8f3e2bc84996e84161cf8afc2c19c
SX <50966843+S-S-X@users.noreply.github.com>
2021-01-18 15:07:57 +0200

    Add continuation marks for external message line feeds

--- cleaner.lua
+++ cleaner.lua
@@ -2,3 +2,7 @@
 beerchat.register_callback('on_receive', function(msg_data)
 	msg_data.message = msg_data.message:gsub('%c','')
 end)
+
+beerchat.register_callback('on_http_receive', function(msg_data)
+	msg_data.message = msg_data.message:gsub('\n','\n  > ')
+end)
