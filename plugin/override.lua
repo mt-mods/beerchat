a80c1b82097c42a3dac94073015b2d54b32711ee
SX <sx@minetest>
2021-01-20 21:48:37 +0200

    Callback handler will send info messages if needed

--- plugin/override.lua
+++ plugin/override.lua
@@ -9,12 +9,8 @@ minetest.register_on_mods_loaded(function()
 		local tell = minetest.registered_chatcommands.tell.func
 		minetest.registered_chatcommands.tell.func = function(name, param)
 			local target, message = param:match("^([^%s]+)%s+(.*)$")
-			local cb_result, cb_message = beerchat.execute_callbacks('before_send_pm', name, message, target)
-			if cb_result then
+			if beerchat.execute_callbacks('before_send_pm', name, message, target) then
 				return tell(name, param)
-			end
-			if cb_message then
-				minetest.chat_send_player(name, cb_message)
 			end
 		end
 	end
