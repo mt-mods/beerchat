ae40204c57161a8389e3d771802912c544f224c0
SX <50966843+S-S-X@users.noreply.github.com>
2021-05-11 17:02:18 +0300

    Password validation plugin

---
+++ plugin/password.lua
@@ -0,0 +1,54 @@
+
+local default_password = minetest.settings:get("default_password")
+if not default_password then
+	-- Do not use if default password is not set
+	return
+end
+
+local only_new_players = minetest.settings:get("beerchat.password.mode") == "new"
+local message = minetest.settings:get("beerchat.password.message") or "*** Please change your password"
+
+local password_notify = {}
+local auth_handler = minetest.get_auth_handler()
+
+local function validate_password(name)
+	local auth_data = auth_handler.get_auth(name)
+	if auth_data and auth_data.password then
+		return not (minetest.check_password_entry(name, auth_data.password, default_password)
+			or minetest.check_password_entry(name, auth_data.password, ""))
+	end
+	return true
+end
+
+beerchat.register_callback('after_joinplayer', function(player, last_login)
+	if not only_new_players or last_login == nil then
+		local name = player:get_player_name()
+		if not validate_password(name) then
+			password_notify[name] = minetest.get_us_time()
+			minetest.chat_send_player(name, "\n" .. message)
+		end
+	end
+end)
+
+beerchat.register_callback('before_send', function(name)
+	-- Only run checks if account is marked for notifications
+	if password_notify[name] then
+		if minetest.get_us_time() - password_notify[name] > 4000000 then
+			-- If marked at login do validation again every 4 seconds
+			if validate_password(name) then
+				-- Password was updated and is now valid, stop complaining
+				password_notify[name] = nil
+				return
+			else
+				password_notify[name] = minetest.get_us_time()
+			end
+		end
+		-- Complain about bad password, this can continue for few seconds after password change
+		minetest.chat_send_player(name, message)
+	end
+end)
+
+minetest.register_on_leaveplayer(function(player)
+	local name = player:get_player_name()
+	password_notify[name] = nil
+end)
