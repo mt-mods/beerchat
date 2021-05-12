528a8a495121d707a0b7664032dc31b10917fbcc
SX <50966843+S-S-X@users.noreply.github.com>
2021-05-12 07:48:12 +0300

    Add some delay before password checks

--- plugin/password.lua
+++ plugin/password.lua
@@ -7,6 +7,7 @@ end
 
 local only_new_players = minetest.settings:get("beerchat.password.mode") == "new"
 local show_formspec = minetest.settings:get_bool("beerchat.password.use_form", true)
+local initial_delay = tonumber(minetest.settings:get("beerchat.password.delay")) or 60
 local message = minetest.settings:get("beerchat.password.message") or "*** Please change your password"
 local fs_message = minetest.formspec_escape(message)
 
@@ -21,22 +22,19 @@ local function validate_password(name)
 	return true
 end
 
-beerchat.register_callback('after_joinplayer', function(player, last_login)
-	if not only_new_players or last_login == nil then
-		local name = player:get_player_name()
-		if not validate_password(name) then
-			password_notify[name] = minetest.get_us_time()
-			minetest.chat_send_player(name, "\n" .. message)
-			if last_login and show_formspec then
-				-- Formspec notification if player has not changed password during first login
-				minetest.show_formspec(name, "MT_PAUSE_MENU",
-					"formspec_version[3]size[14,3]bgcolor[#66F;both;]button_exit[0.5,0.5;13,1;ok;I will]"
-					.. "style_type[*;textcolor=#F00;font_size=*1.2]label[0.5,2;".. fs_message .."]"
-				)
-			end
+local function handle_player(name, last_login)
+	if minetest.get_player_by_name(name) and not validate_password(name) then
+		password_notify[name] = minetest.get_us_time()
+		minetest.chat_send_player(name, "\n" .. message)
+		if last_login and show_formspec then
+			-- Formspec notification if player has not changed password during first login
+			minetest.show_formspec(name, "MT_PAUSE_MENU",
+				"formspec_version[3]size[14,3]bgcolor[#66F;both;]button_exit[0.5,0.5;13,1;ok;I will]"
+				.. "style_type[*;textcolor=#F00;font_size=*1.2]label[0.5,2;".. fs_message .."]"
+			)
 		end
 	end
-end)
+end
 
 beerchat.register_callback('before_send', function(name)
 	-- Only run checks if account is marked for notifications
@@ -53,6 +51,13 @@ beerchat.register_callback('before_send', function(name)
 		end
 		-- Complain about bad password, this can continue for few seconds after password change
 		minetest.chat_send_player(name, message)
+	end
+end)
+
+beerchat.register_callback('after_joinplayer', function(player, last_login)
+	if not only_new_players or last_login == nil then
+		local name = player:get_player_name()
+		minetest.after(initial_delay, function() handle_player(name, last_login) end)
 	end
 end)
 
