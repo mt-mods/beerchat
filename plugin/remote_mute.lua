49554e588283a17b7dc678514fbf588f8ee33e6b
SX <50966843+S-S-X@users.noreply.github.com>
2022-05-27 20:14:52 +0300

    Maybe fix luacheck and indentation?

--- plugin/remote_mute.lua
+++ plugin/remote_mute.lua
@@ -4,14 +4,16 @@ minetest.register_chatcommand("remote_mute", {
 	params = "<Player Name> [<Player Name> ...]",
 	description = ("Mute remote users. Requires %s privilege."):format(beerchat.jail.priv),
 	privs = { [beerchat.jail.priv] = true },
-	func = function(name, param)
-    -- Mute remote player
-    local names = string.gmatch(param, "[^%s,]+")
-    for name in names do
-      remote_muted[name] = true
-    end
-    return true
-  end
+	func = function(_, param)
+		if not param or param == "" then
+			return false, "ERROR: Invalid number of arguments. Please supply the player name(s)."
+		end
+		local names = string.gmatch(param, "[^%s,]+")
+		for remote_name in names do
+			remote_muted[remote_name] = true
+		end
+		return true
+	end
 })
 
 minetest.register_chatcommand("remote_unmute", {
@@ -19,28 +21,28 @@ minetest.register_chatcommand("remote_unmute", {
 	description = ("Unmute remote users or list muted remote users. Requires %s privilege."):format(beerchat.jail.priv),
 	privs = { [beerchat.jail.priv] = true },
 	func = function(name, param)
-    if not param or param == "" then
-      local names = {}
-      for name,_ in pairs(remote_muted) do
-        table.insert(names, name)
-      end
-      if #names > 0 then
-        minetest.chat_send_player(name, "Muted remote users: " .. table.concat(names, ", "))
-      else
-        minetest.chat_send_player(name, "No muted remote users.")
-      end
-    else
-      local names = string.gmatch(param, "[^%s,]+")
-      for name in names do
-        remote_muted[name] = nil
-      end
-    end
-    return true
-  end
+		if not param or param == "" then
+			local names = {}
+			for remote_name,_ in pairs(remote_muted) do
+				table.insert(names, remote_name)
+			end
+			if #names > 0 then
+				minetest.chat_send_player(name, "Muted remote users: " .. table.concat(names, ", "))
+			else
+				minetest.chat_send_player(name, "No muted remote users.")
+			end
+		else
+			local names = string.gmatch(param, "[^%s,]+")
+			for remote_name in names do
+				remote_muted[remote_name] = nil
+			end
+		end
+		return true
+	end
 })
 
 beerchat.register_callback('on_http_receive', function(msg_data)
 	if msg_data.name and remote_muted[msg_data.name] then
-    return false
-  end
+		return false
+	end
 end)
